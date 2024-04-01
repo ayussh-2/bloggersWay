02a7ab5b09f54e3f15a73c21e3a1979dbc497aa3
ayussh-2 <ayushatschool5@gmail.com>
2024-04-02 02:03:19 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ const app = express();
 const userAuthRoutes = require("./routes/userAuth");
 const blogPostRoutes = require("./routes/blogPost");
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://example.com",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+);
 app.use("/api/users", userAuthRoutes);
 app.use("/api/blogs", blogPostRoutes);
 
